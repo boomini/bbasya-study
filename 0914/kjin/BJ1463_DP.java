fdf5dacec4cb3d2441227dcf8fbf696e4d2387c9
kjin41 <51227769+kjin41@users.noreply.github.com>
2021-09-14 17:22:58 +0900

    Add files via upload

---
+++ 0914/kjin/BJ1463_DP.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ1463_DP {
+// baekjoon 1463. 1로 만들기 
+// dp
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		System.setIn(new FileInputStream("input1463.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int dp[] = new int[N+1];
+		for (int i=2; i<=N; i++) {
+			dp[i]=Integer.MAX_VALUE;
+		}
+		for (int i=1; i<N; i++) {
+			if (i*3<=N)
+				dp[i*3] = Math.min(dp[i*3], dp[i]+1);
+			if (i*2<=N)
+				dp[i*2] = Math.min(dp[i*2], dp[i]+1);
+			dp[i+1] = Math.min(dp[i+1], dp[i]+1);
+		}
+		System.out.println(dp[N]);
+	}
+
+}
